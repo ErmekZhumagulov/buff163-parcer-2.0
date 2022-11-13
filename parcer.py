9a2b9a80645385ed2655f241dd5e6be6dc845e91
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-11-13 23:31:56 +0600

    before exe

--- parcer.py
+++ parcer.py
@@ -8,58 +8,46 @@ from PyQt6.QtWidgets import QApplication, QFileDialog, QMessageBox, QWidget, QTe
 import time
 from selenium.webdriver.common.keys import Keys
 
-Form, Window = uic.loadUiType("untitled.ui")
-
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-errorWindow = QMessageBox()
-
 converter = CurrencyConverter()
 
-def pushed():
+path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
+browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+
+browser.get('https://buff.163.com/market/csgo#tab=selling&page_num=1')
+
+links = []
+for i in range(1, 21):
+    item = browser.find_element(By.CLASS_NAME, 'market-card > div > ul > li:nth-child(' + str(i) + ') > h3 > a')
+    links.append(item.get_attribute('href'))
+browser.close()
+
+for i in links:
     path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
     browser = webdriver.Chrome(executable_path=path_to_chromedriver)
 
-    browser.get('https://buff.163.com/market/csgo#tab=selling&page_num=' + form.textEdit.toPlainText())
+    browser.get(i)
 
-    links = []
-    for i in range(1, 21):
-        item = browser.find_element(By.CLASS_NAME, 'market-card > div > ul > li:nth-child(' + str(i) + ') > h3 > a')
-        links.append(item.get_attribute('href'))
-    browser.close()
+    itemName = browser.find_element(By.CLASS_NAME, 'detail-cont > div:nth-child(1) > h1').text
 
-    for i in links:
-        path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
-        browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+    steamPriceStr = browser.find_element(By.CLASS_NAME, 'hide-usd').text
+    steamPriceStrRemoveLastOne = steamPriceStr[:-1]
+    steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
+    steamPrice = float(steamPriceStrRemoved)
 
-        browser.get(i)
+    itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
+    itemPriceStrRemoved = itemPriceStr[2:]
+    itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
 
-        itemName = browser.find_element(By.CLASS_NAME, 'detail-cont > div:nth-child(1) > h1').text
+    finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
 
-        steamPriceStr = browser.find_element(By.CLASS_NAME, 'hide-usd').text
-        steamPriceStrRemoveLastOne = steamPriceStr[:-1]
-        steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
-        steamPrice = float(steamPriceStrRemoved)
+    steamPricePrepared = str(steamPrice).replace(".", ",")
+    itemPricePrepared = str(itemPrice).replace(".", ",")
+    finalPercentagePrepared = str(finalPercentage).replace(".", ",")
 
-        itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
-        itemPriceStrRemoved = itemPriceStr[2:]
-        itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
+    with open(r'oxis.csv', 'a') as file:
+        file.write(
+            itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + i + '\n')
 
-        finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
+    browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
 
-        steamPricePrepared = str(steamPrice).replace(".", ",")
-        itemPricePrepared = str(itemPrice).replace(".", ",")
-        finalPercentagePrepared = str(finalPercentage).replace(".", ",")
-
-        with open(r'oxis.csv', 'a') as file:
-            file.write(
-                itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + i + '\n')
-
-        browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
-
-form.pushButton.clicked.connect(pushed)
-
-app.exec()
+browser.close()
