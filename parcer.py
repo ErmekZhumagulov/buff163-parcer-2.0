e0f1c9ac15fcbde66ded6b8d217115ab9f1c56ef
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-11-20 05:32:41 +0600

    fixing elements exists and adding time to csv

--- parcer.py
+++ parcer.py
@@ -7,11 +7,12 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMessageBox, QWidget, QTextEdit
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 converter = CurrencyConverter()
 
 path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
-browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 browser.get('https://buff.163.com/market/csgo#tab=selling&page_num=1')
 
@@ -19,36 +20,39 @@ links = []
 for i in range(1, 21):
     item = browser.find_element(By.CLASS_NAME, 'market-card > div > ul > li:nth-child(' + str(i) + ') > h3 > a')
     links.append(item.get_attribute('href'))
-browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
 
 for i in links:
-    path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
-    browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+    browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
     browser.get(i)
 
     itemName = browser.find_element(By.CLASS_NAME, 'detail-cont > div:nth-child(1) > h1').text
 
-    steamPriceStr = browser.find_element(By.CLASS_NAME, 'hide-usd').text
-    steamPriceStrRemoveLastOne = steamPriceStr[:-1]
-    steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
-    steamPrice = float(steamPriceStrRemoved)
+    try:
+        steamPriceStr = browser.find_element(By.CLASS_NAME, 'hide-usd').text
+        steamPriceStrRemoveLastOne = steamPriceStr[:-1]
+        steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
+        steamPrice = float(steamPriceStrRemoved)
 
-    if steamPrice < 200.00:
-        itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
-        itemPriceStrRemoved = itemPriceStr[2:]
-        itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
+        if steamPrice < 200.00:
+            itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
+            itemPriceStrRemoved = itemPriceStr[2:]
+            itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
 
-        finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
+            finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
 
-        steamPricePrepared = str(steamPrice).replace(".", ",")
-        itemPricePrepared = str(itemPrice).replace(".", ",")
-        finalPercentagePrepared = str(finalPercentage).replace(".", ",")
+            steamPricePrepared = str(steamPrice).replace(".", ",")
+            itemPricePrepared = str(itemPrice).replace(".", ",")
+            finalPercentagePrepared = str(finalPercentage).replace(".", ",")
 
-        with open(r'oxis.csv', 'a') as file:
-            file.write(itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + i + '\n')
-    else:
-        pass
+            timePrepared = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+            with open(r'oxis.csv', 'a') as file:
+                file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + i + '\n')
+        else:
+            pass
+    except NoSuchElementException:
+        print("Element does not exist")
 
     browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
 
