6ed863d5970fb90f87b1912c1f32b95a1a3a9eca
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-12-04 04:24:21 +0600

    v1.0

--- parcer.py
+++ parcer.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from currency_converter import CurrencyConverter
 from datetime import date, datetime
 import csv
+import os.path
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMessageBox, QWidget, QTextEdit
 import time
@@ -11,7 +12,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 converter = CurrencyConverter()
 
-path_to_chromedriver = 'D:\work\buff163-parcer-main-2\chromedriver.exe'
+path_to_chromedriver = 'D:\work\buff163-parcer-2\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 browser.get('https://buff.163.com/market/csgo#tab=selling&page_num=1')
@@ -34,25 +35,37 @@ for i in links:
         steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
         steamPrice = float(steamPriceStrRemoved)
 
-        if steamPrice < 200.00:
+        if steamPrice < 700.00:
             itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
             itemPriceStrRemoved = itemPriceStr[2:]
             itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
 
             finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
+            finalPercentageStB = round(((itemPrice - itemPrice / 100 * 2.5) * 100 / steamPrice) - 100, 2)
 
             steamPricePrepared = str(steamPrice).replace(".", ",")
             itemPricePrepared = str(itemPrice).replace(".", ",")
             finalPercentagePrepared = str(finalPercentage).replace(".", ",")
+            finalPercentageStBPrepared = str(finalPercentageStB).replace(".", ",")
 
             timePrepared = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
+            file_exists = os.path.isfile('oxis.csv')
+
             with open(r'oxis.csv', 'a') as file:
-                file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + i + '\n')
+                headers = ['time', 'item', 'steam price, $', 'buff163 price, $', 'b>s income, %', 's>b income, %', 'link']
+                writer = csv.DictWriter(file, delimiter=';', lineterminator='\n', fieldnames=headers)
+                if not file_exists:
+                    writer.writeheader()
+
+                try:
+                    file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + finalPercentageStBPrepared + ';' + i + '\n')
+                except UnicodeEncodeError:
+                    print('codec error')
         else:
             pass
     except NoSuchElementException:
-        print("Element does not exist")
+        print('Element does not exist')
 
     browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
 
