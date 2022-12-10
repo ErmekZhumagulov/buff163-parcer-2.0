a6f23454c15d18ae99b22db5f6df411dbbe7d9af
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-12-11 03:54:17 +0600

    specific link in ui completed

--- parcer.py
+++ parcer.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import csv
 import os.path
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMessageBox
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
@@ -19,6 +19,26 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
+errorWindow = QMessageBox()
+
+def errors(text):
+    errorWindow.setWindowTitle('Error')
+    errorWindow.setText(text)
+    errorWindow.setIcon(QMessageBox.Icon.Warning)
+    errorWindow.setStandardButtons(QMessageBox.StandardButton.Ok)
+    errorWindow.exec()
+
+    opt = Options()
+    opt.add_experimental_option("debuggerAddress", "localhost:8989")
+    path_to_chromedriver = 'chromedriver.exe'
+    browser = webdriver.Chrome(executable_path=path_to_chromedriver, options=opt)
+
+    browser.get('https://buff.163.com/market/steam_inventory')
+    browser.refresh()
+
+    # delay
+    html_page = urllib.request.urlopen('https://google.com')
+    soup = BeautifulSoup(html_page, 'html.parser')
 
 converter = CurrencyConverter()
 
@@ -43,8 +63,11 @@ def categoryGroupAll():
 
         links = []
         for i in range(1, 21):
-            item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
-            links.append(item.get_attribute('href'))
+            try:
+                item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
+                links.append(item.get_attribute('href'))
+            except NoSuchElementException:
+                pass
 
         for i in links:
             browser = webdriver.Chrome(executable_path=path_to_chromedriver)
@@ -88,11 +111,11 @@ def categoryGroupAll():
                         try:
                             file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + finalPercentageStBPrepared + ';' + i + '\n')
                         except UnicodeEncodeError:
-                            print('codec error')
+                            errors('Codec error')
                 else:
                     pass
             except NoSuchElementException:
-                print('Element does not exist')
+                errors('Element does not exist')
 
             browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
         # browser.close()
@@ -118,8 +141,11 @@ def categoryGroupStickers():
 
         links = []
         for i in range(1, 21):
-            item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
-            links.append(item.get_attribute('href'))
+            try:
+                item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
+                links.append(item.get_attribute('href'))
+            except NoSuchElementException:
+                pass
 
         for i in links:
             browser = webdriver.Chrome(executable_path=path_to_chromedriver)
@@ -163,11 +189,11 @@ def categoryGroupStickers():
                         try:
                             file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + finalPercentageStBPrepared + ';' + i + '\n')
                         except UnicodeEncodeError:
-                            print('codec error')
+                            errors('Codec error')
                 else:
                     pass
             except NoSuchElementException:
-                print('Element does not exist')
+                errors('Element does not exist')
 
             browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
         # browser.close()
@@ -193,8 +219,11 @@ def categoryGroupOther():
 
         links = []
         for i in range(1, 21):
-            item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
-            links.append(item.get_attribute('href'))
+            try:
+                item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
+                links.append(item.get_attribute('href'))
+            except NoSuchElementException:
+                pass
 
         for i in links:
             browser = webdriver.Chrome(executable_path=path_to_chromedriver)
@@ -238,17 +267,93 @@ def categoryGroupOther():
                         try:
                             file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + finalPercentageStBPrepared + ';' + i + '\n')
                         except UnicodeEncodeError:
-                            print('codec error')
+                            errors('Codec error')
                 else:
                     pass
             except NoSuchElementException:
-                print('Element does not exist')
+                errors('Element does not exist')
 
             browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
         # browser.close()
 
+def specificLink():
+    opt = Options()
+    opt.add_experimental_option("debuggerAddress", "localhost:8989")
+    path_to_chromedriver = 'chromedriver.exe'
+    browser = webdriver.Chrome(executable_path=path_to_chromedriver, options=opt)
+
+    try:
+        browser.get(form.textEdit_link.toPlainText())
+        browser.refresh()
+    except:
+        errors('Wrong link')
+
+    # delay
+    html_page = urllib.request.urlopen('https://google.com')
+    soup = BeautifulSoup(html_page, 'html.parser')
+
+    links = []
+    for i in range(1, 21):
+        try:
+            item = browser.find_element(By.CLASS_NAME, 'card_csgo > li:nth-child(' + str(i) + ') > a')
+            links.append(item.get_attribute('href'))
+        except NoSuchElementException:
+            pass
+
+    for i in links:
+        browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+
+        browser.get(i)
+
+        itemName = browser.find_element(By.CLASS_NAME, 'detail-cont > div:nth-child(1) > h1').text
+
+        try:
+            steamPriceStr = browser.find_element(By.CLASS_NAME, 'hide-usd').text
+            steamPriceStrRemoveLastOne = steamPriceStr[:-1]
+            steamPriceStrRemoved = steamPriceStrRemoveLastOne[3:]
+            steamPrice = float(steamPriceStrRemoved)
+
+            if steamPrice < 700.00:
+                try:
+                    itemPriceStr = browser.find_element(By.CLASS_NAME, 'list_tb_csgo > tr:nth-child(2) > td:nth-child(5)').text
+                    itemPriceStrRemoved = itemPriceStr[2:]
+                    itemPrice = round(converter.convert(float(itemPriceStrRemoved), 'CNY', 'USD'), 2)
+                except ValueError:
+                    pass
+
+                finalPercentage = round(((steamPrice - steamPrice / 100 * 15) * 100 / itemPrice) - 100, 2)
+                finalPercentageStB = round(((itemPrice - itemPrice / 100 * 2.5) * 100 / steamPrice) - 100, 2)
+
+                steamPricePrepared = str(steamPrice).replace(".", ",")
+                itemPricePrepared = str(itemPrice).replace(".", ",")
+                finalPercentagePrepared = str(finalPercentage).replace(".", ",")
+                finalPercentageStBPrepared = str(finalPercentageStB).replace(".", ",")
+
+                timePrepared = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+                file_exists = os.path.isfile('parced-data.csv')
+
+                with open(r'parced-data.csv', 'a') as file:
+                    headers = ['time', 'item', 'steam price, $', 'buff163 price, $', 'b>s income, %', 's>b income, %', 'link']
+                    writer = csv.DictWriter(file, delimiter=';', lineterminator='\n', fieldnames=headers)
+                    if not file_exists:
+                        writer.writeheader()
+
+                    try:
+                        file.write(timePrepared + ';' + itemName + ';' + steamPricePrepared + ';' + itemPricePrepared + ';' + finalPercentagePrepared + ';' + finalPercentageStBPrepared + ';' + i + '\n')
+                    except UnicodeEncodeError:
+                        errors('Codec error')
+            else:
+                pass
+        except NoSuchElementException:
+            errors('Element does not exist')
+
+        browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
+    # browser.close()
+
 form.btn_all.clicked.connect(categoryGroupAll)
 form.btn_stickers.clicked.connect(categoryGroupStickers)
 form.btn_others.clicked.connect(categoryGroupOther)
+form.btn_link.clicked.connect(specificLink)
 
 app.exec()
